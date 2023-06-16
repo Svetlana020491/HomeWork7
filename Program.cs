8b8cd9a25ce5758ae082a0917e3290dca22dffb7
Svetlana Golubeva <svetlana020491@gmail.com>
2023-06-17 02:19:59 +0500

    2 задачи из 3 решены

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿void FillArray(int[] array) //заполнение массива ранодмными числами 
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array) // вывод массива на экран
+{
+    Console.WriteLine($"Вывод массива");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($" ");
+}
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayMatrix(int[,] matrix) // заполнение двумерного массива 
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+void PrintArrayMatrix(int[,] matrix) // печать двумерного массива 
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+void FillMatrix(double[,] matrix)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.NextDouble() + random.Next(-10, 10);
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]:F1} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Task47()// готова
+{
+    //Задайте двумерный массив 
+    //размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
+
+    // m×n обозначила как int rows и int columns
+
+    int rows = Input("Введите количество строк матрицы: ");
+    int columns = Input("Введите количество столбцов матрицы: ");
+    double[,] matrix = new double[rows, columns];
+
+    FillMatrix(matrix);
+    PrintMatrix(matrix);
+
+}
+
+void Task50()// готова
+{
+    // Задача 50. Напишите программу, которая на вход принимает индексы 
+    // элемента в двумерном массиве, и возвращает значение этого элемента 
+    // или же указание, что такого элемента нет.
+
+    int rows = Input("Введите количество строк матрицы: ");
+    int columns = Input("Введите количество столбцов матрицы: ");
+    int[,] matrix = new int[rows, columns];
+    FillArrayMatrix(matrix);
+    PrintArrayMatrix(matrix);
+
+    int i = Input("Введите индекс I:  ");
+    int j = Input("Введите индекс J:  ");
+    if (i > rows && j > columns)
+        Console.WriteLine("такого элемента в массиве нет");
+    else
+        Console.WriteLine($" matrix [{i}, {j}] -> {matrix.GetValue(i, j)} ");
+}
+
+void Task52()
+{
+    // Задача 52. Задайте двумерный массив из целых чисел. 
+    // Найдите среднее арифметическое элементов в каждом столбце.
+
+    int rows = Input("Введите количество строк матрицы: ");
+    int columns = Input("Введите количество столбцов матрицы: ");
+    int[,] matrix = new int[rows, columns];
+    FillArrayMatrix(matrix);
+    PrintArrayMatrix(matrix);
+
+
+
+    Console.WriteLine();
+
+}
+
+Console.Clear();
+Task52();
+
